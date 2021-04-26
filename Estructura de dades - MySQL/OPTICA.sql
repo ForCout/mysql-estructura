29f3ced0ab9eb02f0e8eb7dbc04a911cf2f8c427
ForCout <77494173+ForCout@users.noreply.github.com>
2021-04-26 12:09:31 +0200

    Update OPTICA.sql

--- Estructura de dades - MySQL/OPTICA.sql
+++ Estructura de dades - MySQL/OPTICA.sql
@@ -1,5 +1,5 @@
 
-CREATE SCHEMA IF NOT EXISTS `optica` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+CREATE SCHEMA IF NOT EXISTS `optica` ;
 USE `optica` ;
 
 -- -----------------------------------------------------
